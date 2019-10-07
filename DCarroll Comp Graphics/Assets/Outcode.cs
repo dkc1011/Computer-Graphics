97083e80355cfa864428ea1b89f6c5717a9acf19
Daragh K. Carroll <Daragh.K.Carroll@students.ittralee.ie>
2019-10-07 15:48:47 +0100

    Created Outcode class and driver for clipping 07 - 10 - 2019
    Signed-off-by: Daragh K. Carroll <Daragh.K.Carroll@students.ittralee.ie>

---
+++ DCarroll Comp Graphics/Assets/Outcode.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Outcode
+{
+    public bool up;
+    public bool down;
+    public bool left;
+    public bool right;
+
+    public Outcode()
+    {
+        up = false;
+        down = false;
+        right = false;
+        left = false;
+    }
+
+    public Outcode(Vector2 v)
+    {
+        up = v.y > 1;
+        down = v.y < -1;
+        left = v.x < -1;
+        right = v.x > 1;
+    }
+
+    public Outcode(bool Up, bool Down, bool Left, bool Right)
+    {
+        up = Up;
+        down = Down;
+        left = Left;
+        right = Right;
+    }
+
+    //Logical EQUALS
+    public static bool operator ==(Outcode a, Outcode b)
+    {
+        return (a.up == b.up) && (a.down == b.down) && (a.right == b.right) && (a.left == b.left);
+    }
+
+    //Logical NOT EQUALS
+    public static bool operator !=(Outcode a, Outcode b)
+    {
+        return !(a == b);
+    }
+
+    //Logical AND
+    public static Outcode operator *(Outcode a, Outcode b)
+    {
+        return new Outcode(a.up && b.up, a.down && b.down, a.left && b.left, a.right && b.right);
+    }
+
+    //Logical OR
+    public static Outcode operator +(Outcode a, Outcode b)
+    {
+        return new Outcode(a.up || b.up, a.down || b.down, a.left || b.left, a.right || b.right);
+    }
+
+}
---
+++ DCarroll Comp Graphics/Assets/OutcodeDriver.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutcodeDriver : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public static bool LineClip(ref Vector2 v, ref Vector2 u)
+    {
+        Outcode inViewPort = new Outcode();
+        Outcode vO = new Outcode(v);
+        Outcode uO = new Outcode(u);
+
+        //Detect Trivial Acceptance
+        if((vO + uO) == inViewPort)
+        {
+            return true;
+        }
+
+        //Detect Trivial Rejection
+        if((vO + uO) != inViewPort)
+        {
+            return false;
+        }
+
+        //If vO is in the viewport, invert the points and check again.
+        if(vO == inViewPort)
+        {
+            return LineClip(ref u, ref v);
+        }
+
+        Vector2 v1;
+
+        if (vO.up)
+        {
+            v1 = v;
+
+            v1 = Intercept(u, v, 0);
+            return false;
+        }
+        if (vO.down)
+        {
+            v1 = v;
+
+            v1 = Intercept(u, v, 1);
+            return false;
+        }
+        if (vO.left)
+        {
+            v1 = v;
+
+            v1 = Intercept(u, v, 2);
+            return false;
+        }
+        if(vO.right)
+        {
+             v1 = v;
+            v1 = Intercept(u, v, 3);
+            return false;
+        }
+    }
+
+    private static Vector2 Intercept(Vector2 p1, Vector2 p2, int v2)
+    {
+        //Equation of a line
+        // y2 - y1 m(x - x1)  or x - x1 = (1/m)(y-y1)
+
+        /*
+        
+        m = Slope of the Line
+        Top     y = 1       x= x1 + (1/m)(1-y1) 
+        Bottom  y = -1      x = x1 + (1/m)(-1-y1)
+        Left    x = -1      y = y1 + m(-1 - x1)
+        Right   x = 1       y = y1 + m(1 + x1)
+          
+        */
+        float slope = (p2.y - p1.y) / (p2.x - p1.x);
+
+        if (v2 == 0)
+        {
+            return new Vector2(p1.x + (1 / slope) * (1 - p1.y), 1);
+        }
+
+        if (v2 == 1)
+        {
+            return new Vector2(p1.x + (1 / slope) * (-1 - p1.y), -1);
+        }
+
+        if (v2 == 3)
+        {
+            return new Vector2(-1, p1.y +  slope * (-1 - p1.x));
+        }
+
+        return new Vector2(1, p1.y + slope * (1 - p1.x));
+    }
+}
